6ffb34bd757e194984404f5b4cb451d6996e0fda
Mohammad Sadegh Maleki <masmaleki@gmail.com>
2022-07-11 10:32:26 +0400

    Remove the Manual test from the forms

--- src/resources/views/calc.blade.php
+++ src/resources/views/calc.blade.php
@@ -25,19 +25,11 @@
                 @endif
                 <input type="hidden" name="mode" value="live">
                 <br>
-                <button type="submit" class="btn btn-success">Import Transaction Data</button>
-                <button type="button" class="btn btn-primary" onclick="DoSubmit()" >Use test data</button>
+                <button type="submit" class="btn btn-success">Calculate Fees</button>
             </form>
         </div>
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-
-<script>
-    function DoSubmit(){
-        document.calcForm.mode.value = 'test';
-        document.getElementById("calcForm").submit();
-    }
-</script>
 </body>
 </html>
